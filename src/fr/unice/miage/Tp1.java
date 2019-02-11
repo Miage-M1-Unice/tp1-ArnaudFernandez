73a25e6c5a7b9a69354457a9931d26cbe2d3f9eb
SpyLeChaton <arnaud.fernandez1@gmail.com>
2019-02-11 15:47:15 +0100

    Réponse question 2

--- src/fr/unice/miage/Tp1.java
+++ src/fr/unice/miage/Tp1.java
@@ -1,8 +1,32 @@
 package fr.unice.miage;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+
 public class Tp1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        File file = new File(".");
+        Tp1 exo2 = new Tp1();
+
+        exo2.displayDirectory(file);
+
+    }
+
+    private void displayDirectory(File file)
+    {
+        System.out.println("\n" + file.getName() + " : ");
+
+        for(int i = 0; i < file.listFiles().length; i++) {
+            if(file.listFiles()[i].isDirectory()){
+                displayDirectory(file.listFiles()[i]);
+            }
+            else
+            {
+                System.out.print("[" + file.listFiles()[i].getName() + "], ");
+            }
+        }
     }
 }
