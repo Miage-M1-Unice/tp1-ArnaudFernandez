a88aeab4f5e3883e202d497baa034054f343ac27
SpyLeChaton <arnaud.fernandez1@gmail.com>
2019-02-18 15:32:56 +0100

    Réponse à la question 1.c)

--- src/fr/unice/miage/Tp1.java
+++ src/fr/unice/miage/Tp1.java
@@ -2,7 +2,12 @@ package fr.unice.miage;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Tp1 {
 
@@ -14,6 +19,7 @@ public class Tp1 {
         Tp1 exo2 = new Tp1();
         Tp1 exo3 = new Tp1();
         Tp1 exo3bis = new Tp1();
+        Tp1 exoUnC = new Tp1();
 
 
         //System.out.println("QUESTION 2 : ");
@@ -33,6 +39,18 @@ public class Tp1 {
 
         System.out.println("QUESTION 3ter : (le filtre est xml / la classe est dans la méthode)");
         exo3bis.displayDirectoryWithFilterInMethod(file, 0);
+
+
+        System.out.print("\n\n\n\n");
+
+        System.out.println("QUESTION 1.c)");
+        exoUnC.patternTraining();
+
+
+        System.out.print("\n\n\n\n");
+
+
+        System.out.println("QUESTION 2 ");
 
     }
 
@@ -147,6 +165,36 @@ public class Tp1 {
                 displayDirectoryWithFilterInMethod(elt, tab[0]++);
             }
         }
+    }
+
+    private void patternTraining(){
+
+        String regex = ".*class.*";
+        String chaineAtester1 = "roger est assis dans la class";
+        String chaineAtester2 = "normalement celle ci foire";
+
+        Pattern p = Pattern.compile(regex);
+
+        Matcher m1 = p.matcher(chaineAtester1);
+        Matcher m2 = p.matcher(chaineAtester2);
+
+        System.out.println("La regex est : " + regex + "\n\n");
+
+        // Test de la chaîne 1
+        System.out.println("La phrase \"" + chaineAtester1 + "\" doit être vraie");
+        System.out.print("Execution de matches : ");
+        System.out.print(m1.matches());
+        System.out.print(" // Expected : true");
+
+
+        System.out.println("\n\n");
+
+        // Test de la chaîne 2
+        System.out.println("La phrase \"" + chaineAtester2 + "\" doit être fausse");
+        System.out.print("Execution de matches : ");
+        System.out.print(m2.matches());
+        System.out.print(" // Expected : false");
+
     }
 }
 
